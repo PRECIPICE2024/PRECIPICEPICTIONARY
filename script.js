42a5b34714ae8da1b5b4c34223f41790c1fad98c
PRECIPICE2024 <krishi_parmar2022@cms.ac.in>
2024-08-10 06:38:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const options = [
-    "Option 1", "Option 2", "Option 3", "Option 4", "Option 5",
-    "Option 6", "Option 7", "Option 8", "Option 9", "Option 10",
-    "Option 11", "Option 12", "Option 13", "Option 14", "Option 15",
+    "Italy", "Iceland", "Argentina", "Spain", "France",
+    "New York", "Singapore", "Australia", "South Korea", "Turkey",
+    "Brazil", "United Kingdom", "Canada", "Egypt", "Mexico",
     "Option 16", "Option 17", "Option 18", "Option 19", "Option 20"
 ];
 
